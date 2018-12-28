c849e28105bbe8820669cf1d6871b5a576e095d5
Diego Coutinho <diegocoutinho@gmail.com>
2018-12-27 21:28:47 -0400

    Initial Commit

---
+++ Dog's Age/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dog's Age
+//
+//  Created by Diego Coutinho on 27/12/18.
+//  Copyright © 2018 Diego Coutinho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
